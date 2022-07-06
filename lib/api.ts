11ac6f134db977724e8e2e4cd56ecc38186f9385
joaobr200 <joaoarantesjob@gmail.com>
2022-07-06 08:18:04 -0300

    add chace adpter

--- lib/api.ts
+++ lib/api.ts
@@ -1,10 +1,16 @@
 import axios from "axios";
+import { setupCache } from "axios-cache-adapter";
+
+const cache = setupCache({
+  maxAge: 60 * 60,
+});
 
 const api = axios.create({
   baseURL: "https://www.abibliadigital.com.br/api",
   headers: {
-    Authorizarion: `Bearer ${process.env.NEXT_PUBLIC_API_KEY}`,
+    Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_KEY}`,
   },
+  adapter: cache.adapter,
 });
 
 export default api;
